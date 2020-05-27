971b66326d116186899cb44fc8ea3ab22892d67f
d7p4n4 <bercikezs199999999@gmail.com>
2020-05-27 16:56:57 +0200

    1.20200527.1

---
+++ CSConfluenceAutomationFWWPFLib/AddNewPageRequest.cs
@@ -0,0 +1,21 @@
+﻿using Modul.Final.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSConfluenceAutomationFWWPFLib
+{
+    class AddNewPageRequest : Ac4yServiceRequest
+    {
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public string URL { get; set; }
+        public string SpaceKey { get; set; }
+        public string PageTitle { get; set; }
+        public string ParentPageTitle { get; set; }
+        public string Content { get; set; }
+        public int IdLength { get; set; }
+    }
+}
